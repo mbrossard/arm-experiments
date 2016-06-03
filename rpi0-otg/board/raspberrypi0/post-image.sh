c0d232aebe4f2b642cf3036d72aa501dc90d5d00
Mathias Brossard <mathias@brossard.org>
2016-06-03 07:28:19 +0200

    Modified configuration for bootloader to add dwc2 overlay

---
+++ rpi0-otg/board/raspberrypi0/post-image.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+BOARD_DIR="$(dirname $0)"
+BOARD_NAME="rpi0-otg"
+GENIMAGE_CFG="${BOARD_DIR}/genimage-${BOARD_NAME}.cfg"
+GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+
+# Mark the kernel as DT-enabled
+mkdir -p "${BINARIES_DIR}/kernel-marked"
+${HOST_DIR}/usr/bin/mkknlimg "${BINARIES_DIR}/zImage" \
+	"${BINARIES_DIR}/kernel-marked/zImage"
+
+echo "console=tty1 root=/dev/mmcblk0p2 rootwait" > "${BINARIES_DIR}/rpi-firmware/cmdline.txt"
+grep -qE dwc2 "${BINARIES_DIR}/rpi-firmware/config.txt" || \
+echo "dtoverlay=dwc2" >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+
+rm -rf "${GENIMAGE_TMP}"
+
+genimage                           \
+	--rootpath "${TARGET_DIR}"     \
+	--tmppath "${GENIMAGE_TMP}"    \
+	--inputpath "${BINARIES_DIR}"  \
+	--outputpath "${BINARIES_DIR}" \
+	--config "${GENIMAGE_CFG}"
+
+exit $?
